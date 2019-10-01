1667533b0113e48bdab894863fa6fa0e3bc4dddc
jqiu98 <jq524@nyu.edu>
2019-10-01 15:54:18 -0400

    Initial setup

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+	createCanvas(400,400);
+	background(0);
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+  fill('red')
+  rectMode(CENTER);
+  rect(width/2, height/2, 50, 50);
+}
