69c45374df03bc8bc81ed5b7a95ae86e6a910e9e
jqiu98 <jq524@nyu.edu>
2019-10-04 13:41:56 -0400

    More messing around

--- Monster.js
+++ Monster.js
@@ -1,18 +1,82 @@
 class Monster {
-	constructor(xpos, ypos) {
-		this.x = xpos;
-		this.y = ypos
+	constructor(x, y, r, g, b, a, speed) {
+		this.title = "master";
+		this.x = x;
+		this.y = y;
+		this.timestamp = frameCount;
+		this.tick = 0;
+
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.a = a;
+
+		this.width = 60;
+		this.height = 35;
+
+		this.buddy = null;
+
+		this.currTrail = [this.x, this.y + this.height/2, this.x, this.y + this.height/2];
+		this.allTrail = [];
+		this.allTrail.push(this.currTrail);
+
+		this.speed = speed;
+	}
+
+	displayTrail() {
+		fill(0);
+		strokeWeight(1.5);
+		console.log(this.allTrail);
+		for (let aTrail of this.allTrail) {
+			line(aTrail[0], aTrail[1], aTrail[2], aTrail[3]);
+		}
 	}
 
 	display() {
-		fill(144, 130, 233);
-		ellipse(this.x, this.y, 60, 35);
+		push();
+		translate(this.x, this.y);
+		fill(this.r, this.g, this.b, this.a);
+		ellipse(0, 0, this.width, this.height);
 		fill(255);
-		ellipse(this.x-12, this.y-5, 7, 14);
-		ellipse(this.x+12, this.y-5, 7, 14);
+		ellipse(-this.width/5, -this.height/7, 7, 14);
+		ellipse(this.width/5, -this.height/7, 7, 14);
+		pop();
+
+		if (this.title == "master") this.displayTrail();
+		if (this.tick == 3 && this.buddy == null) this.createBuddy();
+		if (this.buddy != null) this.buddy.display();
+	}
+
+	addTrail() {
+		this.currTrail = [this.x, this.y + this.height/2, this.x, this.y + this.height/2];
+		this.allTrail.push(this.currTrail);
+		++this.tick;
+	}
+
+	createBuddy() {
+		this.buddy = new Monster(this.allTrail[0][0], this.allTrail[0][1], random(0,255), random(0, 255), random(0, 255), 100, this.speed * 2);
+		this.buddy.title = "slave"
+	}
+
+	updateBuddy() {
+		if (this.buddy.a < 255) this.buddy.a += 1;
+		else this.eatTrail();
+	}
+
+	eatTrail() {
+		this.buddy.walk();
+		this.allTrail[0][0] = this.buddy.x;
+		if (this.allTrail[0][0] > this.allTrail[0][2]) {
+			this.allTrail.shift();
+			this.buddy.x = this.allTrail[0][0];
+			this.buddy.y = this.allTrail[0][1];
+		}
 	}
 
 	walk() {
-		this.x += 3;
+		this.x += this.speed;
+		this.currTrail[2] = this.x;
+		if (this.buddy != null) this.updateBuddy();
+
 	}
 }
--- sketch.js
+++ sketch.js
@@ -1,23 +1,38 @@
-let creature;
 let startY;
+let flag;
+let creatures;
 
 function setup() {
 	createCanvas(500, 500);
 	background(220);
 	startY = height/2;
-	creature = new Monster(0, height/2);
+	flag = true;
+	creatures = [];
 }
 
 function draw() {
-	background(220);
-	strokeWeight(2.2);
-	creature.walk();
-	creature.display();
-	fill(0);
-	line(0, startY + 17.5, creature.x, creature.y + 17.5);
-	if (creature.x > width) {
-		creature.x = 0;
-		startY = random(0, height);
-		creature.y = startY;
+	background(240);
+	if (flag) {
+		for (let aCreature of creatures) {
+			aCreature.walk();
+			if (aCreature.x > width+50) {
+				aCreature.x = 0;
+				startY = random(0, height);
+				aCreature.y = startY;
+				aCreature.addTrail();
+			}
+		}
 	}
+	for (let aCreature of creatures) {
+			aCreature.display();
+	}
+}
+
+function keyPressed() {
+	flag = !flag;
+}
+
+function mousePressed() {
+	let aCreature = new Monster(mouseX, mouseY, 144, 130, 233, 255, int(random(2,4)));
+	creatures.push(aCreature);
 }
